e9e04720e0f32c96e962b554544343294fab3b38
hugowest <mark_s_butler@yahoo.co.uk>
2018-02-12 20:33:01 +0000

    Add sun logic, controller and unit test

---
+++ src/main/java/com/computenow/calculator/CalcualtorController.java
@@ -0,0 +1,18 @@
+package com.computenow.calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalcualtorController {
+    @Autowired
+    private Calculator calculator;
+
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a,
+               @RequestParam("b") Integer b) {
+        return String.valueOf(calculator.sum(a,b));
+    }
+}
