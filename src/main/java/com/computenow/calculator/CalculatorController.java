dd8e73ffb2c367d4a7c19ba25f99c20adab568f9
hugowest <mark_s_butler@yahoo.co.uk>
2018-02-13 14:50:54 +0000

    Added chekstyle static code analysis stage

--- src/main/java/com/computenow/calculator/CalculatorController.java
+++ src/main/java/com/computenow/calculator/CalculatorController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * CalculatorController
+ */
 @RestController
 public class CalculatorController {
     @Autowired
